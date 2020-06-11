d342085495677b81ed4383c40945e2b36bda25bc
ivangm13 <ivangarcia346@gmail.com>
2020-06-11 22:31:56 +0200

    utilizado moment para las fechas

--- apiEmpresa/app.js
+++ apiEmpresa/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressValidator = require('express-validator');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,17 +21,19 @@ require('./db').connect();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 
+app.use('/', indexRouter);
+app.use('/empleados', empleadosRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
-app.use('/empleados', empleadosRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- apiEmpresa/models/empleado.js
+++ apiEmpresa/models/empleado.js
@@ -1,14 +1,48 @@
 
 
+
 const getAll = () => {
-    return new Promise((resolve, reject) => {
-        db.query('select * from empleados', (err, rows) => {
+    return new Promise((resolve, reject) => { 
+        db.query('select * from empleados', (err, rows) => {    
             if (err) reject(err);
             resolve(rows);
         });
     });
 }
 
+ const crear = ({ nombre, dni, sexo, fecha_nacimiento, fecha_incorporación, salario, cargo, departamento, jefe }) => {
+    return new Promise((resolve, reject) => {
+        
+        db.query('insert into empleados (nombre,dni,sexo,fecha_nacimiento,fecha_incorporacion,salario,cargo,fk_departamento,jefe_id) values (?,?,?,?,?,?,?,?,?)',
+            [nombre, dni, sexo, fecha_nacimiento, new Date(), salario, cargo, departamento, jefe],
+            (err, result) => {
+                if (err) reject(err);
+                resolve(result);
+            })
+    });
+}
+const getById=(pIdEmpleado)=>{
+    return new Promise((resolve,reject)=>{
+        db.query('select * from empleados where id = ?',[pIdEmpleado],(err,rows)=>{
+            if(err) reject(err);
+            if(rows.length !==1) reject('El id no existe');
+            resolve(rows[0]);
+        })
+    });
+}
+
+const deleteById = (pIdEmpleado) => {
+    return new Promise((resolve, reject) => {
+        db.query('delete from empleados where id = ?', [pIdEmpleado], (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
+    });
+} 
 module.exports = {
-    getAll
+    getAll,
+     crear,
+    getById,
+    deleteById 
+
 }
