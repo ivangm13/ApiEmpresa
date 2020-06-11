d4c8d032b7f11877d65eb4313e36c73c2ee6cd86
ivangm13 <ivangarcia346@gmail.com>
2020-06-11 22:33:07 +0200

    Realizadas todas las funciones del apartado empleados, a excepción de la validación de los datos introducidos

--- apiEmpresa/routes/api/empleados.js
+++ apiEmpresa/routes/api/empleados.js
@@ -1,3 +1,16 @@
 const router = require('express').Router();
 
+const Empleado = require('../../models/empleado');
+
+
+router.get('/', (req, res) => {
+    console.log(req.payload.empleadoId);
+    Empleado.getAll()
+        .then((rows) => {
+            res.json(rows);
+        })
+        .catch(err => {
+            res.json({ error: err.message });
+        })
+});
 module.exports = router;
--- apiEmpresa/routes/empleados.js
+++ apiEmpresa/routes/empleados.js
@@ -1,16 +1,70 @@
 const router = require('express').Router();
+const moment = require('moment');
+const {check,validationResult} = require('express-validator');
 
 const Empleado = require('../models/empleado');
 
-router.get('/',(req,res)=>{
-    Empleado.getAll()
-    .then(rows=>{
-        console.log(rows);
-        res.render('empleados/index2.html',{empleados: rows});
-    })
-    .catch(err=>{
+router.get('/', async (req, res) => {
+    try {
+        const rows = await Empleado.getAll();
+        for (row of rows) {
+            row.fecha_nacimiento = moment(row.fecha_nacimiento).format('DD/MM/YYYY');
+            row.fecha_incorporacion = moment(row.fecha_incorporacion).format('DD/MM/YYYY');
+        }
+        res.render('empleados/index', { empleados: rows });
+    } catch (err) {
         res.send(err);
-    });
+    };
+
+});
+router.get('/crear', (req, res) => {
+    res.render('empleados/formCrear');
+});
+
+router.get('/editar/:idEmpleado', async (req, res) => {
+    try {
+        const empleado = await Empleado.getById(req.params.idEmpleado);
+        empleado.fecha_nacimiento = moment(empleado.fecha_nacimiento).format('DD/MM/YYYY');
+        res.render('empleados/formEditar', { empleado });
+    } catch (err) {
+        res.send(err);
+    }
+});
+router.get('/borrar/:idEmpleado', async (req, res) => {
+    Empleado.deleteById(req.params.idEmpleado)
+        .then(result => {
+            res.redirect('/empleados')
+        })
+        .catch(err => {
+            res.send(err);
+        });
 })
 
+
+
+// Peticiones al servidor
+
+//Peticion al hacer submit en el formulario de creacion de empleado
+router.post('/crear',[
+check('nombre').notEmpty()
+
+] ,async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.redirect('/empleados/crear');
+            
+        }
+        try {
+            const result = await Empleado.crear(req.body)
+            res.redirect('/empleados');
+
+        } catch (err) {
+            res.send(err);
+        }
+    
+        
+    
+
+
+})
 module.exports = router;
