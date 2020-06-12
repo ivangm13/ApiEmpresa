3bfc40937ef6f408dd56c6f0583719b8e2e35a0d
ivangm13 <ivangarcia346@gmail.com>
2020-06-12 20:03:41 +0200

    Realizado todo el manejo de rutas y peticiones  de la API para los departamentos

--- apiEmpresa/routes/api/departamentos.js
+++ apiEmpresa/routes/api/departamentos.js
@@ -2,13 +2,41 @@ const router = require('express').Router();
 
 const Departamento = require('../../models/departamento');
 
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
     Departamento.getAll()
-    .then(rows=>{
-        res.json(rows)
-    })
-    .catch(err=>{
-        res.json({error: 'No se han podido recuperar los departamentos'})
-    })
+        .then(rows => {
+            res.json(rows)
+        })
+        .catch(err => {
+            res.json({ error: 'No se han podido recuperar los departamentos' })
+        })
+});
+
+router.delete('/:idDepartamento', async (req, res) => {
+    const result = await Departamento.eliminarById(req.params.idDepartamento);
+    if (result['affectedRows'] === 1) {
+        res.json({ success: 'Departamento eliminado con éxito' });
+    } else {
+        res.json({ error: 'No se ha podido eliminar el departamento. Verificar id' })
+
+    }
+});
+
+router.post('/', async (req,res)=>{
+    try{
+        const result = await Departamento.crear(req.body);
+        res.json({success: 'Departamento creado con éxito'})
+    }catch(err){
+        res.json({error:'No se ha podido crear el departamento. Verifica los datos introducidos'})
+    }
+})
+
+router.put('/:idDepartamento',async(req,res)=>{
+    try{
+        const result = Departamento.editarById(req.params.idDepartamento,req.body);
+        res.json({success: 'Departamento modificado con éxito'})
+    }catch(err){
+        res.json({error: 'No se ha podido modificar el departamento. Verifica los datos introducidos'})
+    }
 })
 module.exports = router;
--- apiEmpresa/routes/departamentos.js
+++ apiEmpresa/routes/departamentos.js
@@ -3,19 +3,35 @@ const Departamento = require('../models/departamento');
 
 router.get('/', (req, res) => {
     Departamento.getAll()
-    .then((rows)=>{
-      res.render('departamentos/index', {departamentos: rows});  
-    })
-    .catch(err=>{
-        res.send(err)
-    })
-    
+        .then((rows) => {
+            res.render('departamentos/index', { departamentos: rows });
+        })
+        .catch(err => {
+            res.send(err)
+        })
+
 })
-router.get('/crear',(req,res)=>{
+router.get('/crear', (req, res) => {
     res.render('departamentos/formCrear')
 })
+router.get('/borrar/:idDepartamento', async (req, res) => {
+    Departamento.eliminarById(req.params.idDepartamento)
+        .then(result => {
+            res.redirect('/departamentos')
+        })
+        .catch(err => {
+            res.send(err);
+        });
+})
+router.get('/editar/:idDepartamento', async (req, res) => {
+    try {
+        const departamento = await Departamento.getById(req.params.idDepartamento);
+        res.render('departamentos/formEditar', { departamento });
+    } catch (err) {
+        res.send(err)
+    }
 
-
+})
 //Peticiones
 
 router.post('/crear', async (req, res) => {
@@ -26,4 +42,13 @@ router.post('/crear', async (req, res) => {
         res.send(err);
     }
 })
+
+router.post('/editar', async (req, res) => {
+    try {
+        const result = await Departamento.editarById(req.body.idDepartamento, req.body);
+        res.redirect('/departamentos');
+    } catch (err) {
+        res.send(err)
+    }
+});
 module.exports = router;
